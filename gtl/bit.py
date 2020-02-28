98d1e78931f512feacf197f51f4f6254e8dc7034
Puria Nafisi Azizi <puria@dyne.org>
2020-02-28 20:34:51 +0100

    🎉 Initial import

---
+++ gtl/bit.py
@@ -0,0 +1,4 @@
+# -*- coding: utf-8 -*-
+"""
+Tipipigri © 2020.
+"""
---
+++ tests/test_gtl_test.py
@@ -0,0 +1,12 @@
+"""
+Tipi pigri © 2020.
+"""
+
+from gtl import __version__
+
+
+def test_version() -> None:
+    """
+    Check version.
+    """
+    assert __version__ == "0.1.0"
